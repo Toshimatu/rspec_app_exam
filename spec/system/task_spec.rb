361d8ff5a7319844a3dcb55251116d74e7a023df
github Toshimatu <matsuda.akt@gmail.com>
2020-04-29 10:48:28 +0900

    [Fix] Application_helperをrspec_helperで呼び込み

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
-  include ApplicationHelper
 
   let(:project) { create(:project) }
   let(:task) { create(:task, project_id: project.id) }
@@ -63,7 +62,7 @@ RSpec.describe 'Task', type: :system do
 
   describe 'Task編集' do
     context '正常系' do
-      fit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
+      it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
